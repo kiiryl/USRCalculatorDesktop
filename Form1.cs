fbe1034432fddabd01afa98cc620b305e6ce372a
kiiryl <kiirillpoleshchuk@gmail.com>
2025-12-13 14:08:49 +0300

    0.9v

---
+++ Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UsrCalculatorImba
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        // Вспомогательные методы
+        private string TryCalculate(string expression)
+        {
+            var dt = new DataTable();
+            try
+            {
+                return dt.Compute(expression, "").ToString();
+            }
+            catch (Exception)
+            {
+                return "Ошибка!";
+            }
+        }
+        // Основные кнопки
+        private void InputButtonsClick(object sender, EventArgs e)
+        {
+            var currentButton = sender as Button;
+            textBox1.Text += currentButton.Text;
+        }
+
+        private void EqualsButton_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = TryCalculate(textBox1.Text);
+        }
+
+        private void DeleteButton(object sender, EventArgs e)
+        {
+            string str = textBox1.Text;
+            if (string.IsNullOrEmpty(str)) textBox1.Text = "";
+            else textBox1.Text = str.Substring(0, str.Length - 1);
+        }
+
+        private void DeleteAllButton(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+        }
+        private void SwapResultToInput(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox2.Text;
+            textBox2.Text = "";
+        }
+        // Инженерные функции
+        private void SinButton(object sender, EventArgs e)
+        {
+            string result = TryCalculate(textBox1.Text);
+            textBox2.Text = Math.Sin(Convert.ToDouble(result)).ToString();
+        }
+
+        private void CosButton(object sender, EventArgs e)
+        {
+            string result = TryCalculate(textBox1.Text);
+            textBox2.Text = Math.Cos(Convert.ToDouble(result)).ToString();
+        }
+
+        private void SqrtButton(object sender, EventArgs e)
+        {
+            string result = TryCalculate(textBox1.Text);
+            textBox2.Text = Math.Sqrt(Convert.ToDouble(result)).ToString();
+        }
+
+        private void SquaredButton(object sender, EventArgs e)
+        {
+            string result = TryCalculate(textBox1.Text);
+            textBox2.Text = Math.Exp(Convert.ToDouble(result)).ToString();
+        }
+
+
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("UsrCalculatorImba")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UsrCalculatorImba")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("9f6efdef-329c-4aa3-81ec-6bde5a524cc1")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
